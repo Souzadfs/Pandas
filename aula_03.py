042dc8238f4f6f4253c9e40c2f7fd660ef145599
Souzadfs <souzadf09@gmail.com>
2025-09-08 22:02:06 -0300

    learning df

--- aula_03.py
+++ aula_03.py
@@ -2,6 +2,16 @@ import pandas as pd
 import numpy as np
 
 datas = pd.date_range('20250908', periods= 60, freq= "D")
-df = pd.DataFrame(np.random.randn(60,5), index= datas, columns= list("ABCD"))
+df = pd.DataFrame(np.random.randn(60,5), index= datas, columns= list("ABCDE"))
 
-print(df, datas)
+
+
+print(df.head(3))
+
+df['F'] = 1
+df['G'] = range(60)
+df['Produto'] = df['A'] * df['B']
+df['Maximo'] = df['A'].max()
+df['Minimo'] = df['A'].min()
+df['Média'] = df['A'].mean()
+print(df, df.shape)
