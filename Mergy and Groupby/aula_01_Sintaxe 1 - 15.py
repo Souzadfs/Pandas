9457f20dc3634f19e07a84de7cbce767c317e229
Souzadfs <souzadf09@gmail.com>
2025-09-11 22:25:50 -0300

    Criei dois cadastro e registrei compras

--- Mergy and Groupby/aula_01_Sintaxe 1 - 15.py
+++ Mergy and Groupby/aula_01_Sintaxe 1 - 15.py
@@ -11,6 +11,8 @@ cadastro_a = { 'id': ['AA2930', 'BB4563', 'CC254', 'DD7879'],
 cadastro_a = pd.DataFrame(cadastro_a, columns= ['id', 'Nome', 'idade'])
 print(cadastro_a)
 
+print('-------------------------------------------------')
+
 
 # cadastro_b
 
@@ -22,3 +24,20 @@ cadastro_b= { 'id': ['AA74630', 'BB4543', 'CC544', 'DD7949'],
 cadastro_b = pd.DataFrame(cadastro_b, columns=['id', 'Nome', 'idade'])
 print(cadastro_b)
 
+#Registro da compra
+
+print('-------------------------------------------------')
+
+
+# Dados com 10 elementos em cada coluna
+compra = {
+    'id': ['AA74630', 'BB4543', 'CC544', 'DD7949', 'AA2930', 'BB4563', 'CC254', 'DD7879', 'EE1234', 'FF5678'],
+    'data': ['20/08/25', '25/02/24', '30/11/23', '01/01/25', '15/07/24', '10/06/25', '05/05/24', '12/12/23', '08/03/25', '22/09/24'],
+    'valor': [100, 252, 360, 100, 145, 200, 175, 300, 220, 180]
+}
+
+# Criação do DataFrame
+compra = pd.DataFrame(compra, columns=['id', 'data', 'valor'])
+
+# Exibição
+print(compra)
