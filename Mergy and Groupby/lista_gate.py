19470201fd38b71a61ec4024adbf4e1ac1a068dc
Souzadfs <souzadf09@gmail.com>
2025-09-16 14:06:26 -0300

    automatizando um processo

--- Mergy and Groupby/lista_gate.py
+++ Mergy and Groupby/lista_gate.py
@@ -1,8 +1,20 @@
+# import pandas as pd
+
+# df = pd.read_csv("Lista.csv")
+# df.head()
+# df.tail()
+# print(df)
+# colunas= pd.set_option('Display.max_columns', None)
+# print(colunas)
+
+
 import pandas as pd
 
-df = pd.read_csv("Lista.csv")
-df.head()
-df.tail()
-print(df)
-df.set_option('Lista.csv', None)
+# Configura o Pandas para mostrar todas as colunas
+pd.set_option('display.max_columns', None)
+
+# Lê o arquivo CSV
+df = pd.read_csv('Lista.csv')
+
+# Exibe o DataFrame
 print(df)
